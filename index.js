d9d5ef177773c99b27505c3a9d4d244077b397c6
Kunal Mathur <kunal.mathur@tcs.com>
2015-03-04 16:27:25 +0530

    Adding body parser plugin

--- index.js
+++ index.js
@@ -2,10 +2,23 @@
 	* Declaring the required modules for the application.
 */
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+app.use(bodyParser.json());
+
 
 // -- declaring the folder to serve static assets
 app.use(express.static('public'));
+
+/**
+	* API End Points
+*/
+app.post('/courseid', function (req, res) {
+	console.log(req.body.id);
+  res.send("hello");
+});
+
+
 
 // -- code to start the server -- 
 var server = app.listen(3000, function () {
