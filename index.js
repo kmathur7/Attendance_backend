303c58e18a37a26b63c03a5a109adc11004e4833
Kunal Mathur <kunal.mathur@tcs.com>
2015-03-25 00:20:20 +0530

    Adding API to search and fetch courseID to mark attendence against

--- index.js
+++ index.js
@@ -4,8 +4,35 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://kunal:kalp1234@ds051831.mongolab.com:51831/attendancedb');
 app.use(bodyParser.json());
 
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function (callback) {
+  console.log("connected!!");
+});
+
+// -- Defining Schemas 
+var Schema = mongoose.Schema;
+var courseSchema = new Schema({
+  code:  String,
+  title: String,
+  centre: String,
+  venue: String,
+  startdate: Date,
+  enddate: Date,
+  faculty:{
+    empid: Number,
+    name: String,
+    desgn: String,
+    email: String,
+    project: String
+  }
+});
+
+var Course = mongoose.model('Course', courseSchema);
 
 // -- declaring the folder to serve static assets
 app.use(express.static('public'));
@@ -14,10 +41,22 @@ app.use(express.static('public'));
 	* API End Points
 */
 app.post('/courseid', function (req, res) {
-	console.log(req.body.id);
-  res.send("hello");
+  Course.findOne({ code: req.body.id },function(err,coursedata){
+    console.log(coursedata);
+    res.send({content:true,data:coursedata});
+  });
+	
+  
 });
 
+app.post('/newcourse', function (req, res) {
+  //console.log(req.body.code);
+  var newcourse = new Course(req.body);
+  newcourse.save(function (err) {
+    if (err) return console.log(err);
+  // saved!
+  });
+});
 
 
 // -- code to start the server -- 
